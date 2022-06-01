df938179624efc81da6c503265c2c8582198658d
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2022-06-01 09:11:09 +0100

    chore: updated install anna file

--- scripts/install-anna.sh
+++ scripts/install-anna.sh
@@ -13,12 +13,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+#  Modifications copyright (C) 2021 Taras Lykhenko, Rafael Soares
 
 cd
-git config --global user.name "taraslykhenko"
-git config --global user.password "b718ab81fae7cf4476e12ec6b9dd7c23ba3e3517"
-git clone --recurse-submodules https://github.com/TarasLykhenko/AnnaTCC.git
-cd AnnaTCC/client/python
+git clone --recurse-submodules https://github.com/FaaSTCC/annaTCC.git
+cd annaTCC/client/python
 python3 setup.py install
 cd ~
-rm -rf AnnaTCC
+rm -rf annaTCC
